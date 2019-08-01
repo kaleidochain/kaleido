14ff5904eb6d68148b216f9409c9bd71f9f1b1ad
connorbuilder <connorbuilder@keidochain.io>
2019-08-01 10:24:45 +0800

    fix(algorand/state.go): fix NewNodeSet

--- consensus/algorand/core/state.go
+++ consensus/algorand/core/state.go
@@ -123,8 +123,8 @@ func (s *StateBase) OnCertVoteEnough(value common.Hash, round uint32) StateEvent
 		panic("Unexpected error getting cvs or leaderProposalValue")
 	}
 
-	var proof types.NodeList
-	err := BuildProof(s.config.Algorand, s.parentStatedb, height, blockData.Address, cvs, &proof)
+	proof := types.NewNodeSet()
+	err := BuildProof(s.config.Algorand, s.parentStatedb, height, blockData.Address, cvs, proof)
 	if err != nil {
 		log.Error("build proof failed", "err", err)
 		panic(fmt.Sprintf("build proof failed: %v", err))
@@ -137,7 +137,7 @@ func (s *StateBase) OnCertVoteEnough(value common.Hash, round uint32) StateEvent
 		Value:       value,
 		Proposal:    blockData.ToProposalStorage(),
 		CertVoteSet: cvs,
-		TrieProof:   proof,
+		TrieProof:   proof.NodeList(),
 	}
 
 	sealedBlock := blockData.Block.WithSeal(header)
