93a1961eb35222bfc9fa733cf325fad9c04b6265
kellbenet <kb@kaleidochain.io>
2019-08-15 18:13:46 +0800

    refactor(scchain_test): convert to generators

--- consensus/stamping/scchain_test.go
+++ consensus/stamping/scchain_test.go
@@ -3,7 +3,6 @@ package stamping
 import (
 	"math/rand"
 	"testing"
-	"time"
 )
 
 func buildStampingCertificate(height uint64, proofHeader *Header) *StampingCertificate {
@@ -14,29 +13,60 @@ func buildStampingCertificate(height uint64, proofHeader *Header) *StampingCerti
 	return nil
 }
 
-func TestNewChain(t *testing.T) {
-	rand.Seed(time.Now().UTC().UnixNano())
+type block struct {
+	header *Header
+	fc     *FinalCertificate
+}
 
+func makeBlockGenerator(config *Config, chain *Chain, maxHeight uint64) <-chan block {
+	ch := make(chan block)
+	go func() {
+		parent := genesisHeader
+		for height := uint64(1); height < maxHeight; height++ {
+			header := NewHeader(height, parent)
+			fc := NewFinalCertificate(height, parent)
+			ch <- block{header, fc}
+		}
+	}()
+	return ch
+}
+
+const (
+	newBlockEvent = 1
+)
+
+type event struct {
+	Height uint64
+	Type   uint
+}
+
+func makeStampingGenerator(config *Config, chain *Chain, eventCh <-chan event) <-chan *StampingCertificate {
+	ch := make(chan *StampingCertificate)
+	go func() {
+		for e := range eventCh {
+			proofHeader := chain.Header(e.Height - config.B)
+			s := buildStampingCertificate(e.Height, proofHeader)
+			ch <- s
+		}
+	}()
+	return ch
+}
+
+func TestNewChain(t *testing.T) {
+	const maxHeight = 10000
 	chain := NewChain()
 
-	const MaxHeight = 10000
-	parent := genesisHeader
-	for height := uint64(1); height < MaxHeight; height++ {
-		header := NewHeader(height, parent)
-		fc := NewFinalCertificate(height, parent)
+	blockCh := makeBlockGenerator(defaultConfig, chain, maxHeight)
+	eventCh := make(chan event)
+	stampingCh := makeStampingGenerator(defaultConfig, chain, eventCh)
 
-		if err := chain.AddBlock(header, fc); err != nil {
-			t.Errorf("AddBlock error, height:%d, err:%s", height, err)
-		}
-		parent = header
-
-		if height > defaultConfig.B {
-			proofHeader := chain.Header(height)
-
-			sc := buildStampingCertificate(height, proofHeader)
-			if err := chain.AddStampingCertificate(sc); err != nil {
-				t.Errorf("AddStampingCertificate error, height:%d, err:%s", height, err)
-			}
+	for {
+		select {
+		case block := <-blockCh:
+			chain.AddBlock(block.header, block.fc)
+			eventCh <- event{Height: block.header.Height, Type: newBlockEvent}
+		case s := <-stampingCh:
+			chain.AddStampingCertificate(s)
 		}
 	}
 
